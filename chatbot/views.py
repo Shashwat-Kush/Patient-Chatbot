e6272c3c39c8625debda19723014988902c87275
Shashwat-Kush <shashwatkush002@kgpian.iitkgp.ac.in>
2025-06-26 10:48:22 +0530

    Add chat room view and URL routing for WebSocket integration

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+
+def chat_room(request, room_name="lobby"):
+    return render(request, 'templates/chatbot/index.html', { 'room_name': room_name })
--- patient_chatbot/asgi.py
+++ patient_chatbot/asgi.py
@@ -8,9 +8,12 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
-
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+import chatbot.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "patient_chatbot.settings")
-
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": URLRouter(chatbot.routing.websocket_urlpatterns),
+})
